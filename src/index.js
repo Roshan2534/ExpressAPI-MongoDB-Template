54aaf970d2dfadb66bb3b823f16257922ca2fa4e
Roshan2534 <roshansaundankar67@gmail.com>
2020-03-13 01:42:06 -0700

    First commit

---
+++ src/index.js
@@ -0,0 +1,30 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser'); 
+const authRoutes = require('./routes/authRoutes');
+
+const app = express();
+
+app.use(bodyParser.json());
+app.use(authRoutes);
+
+const mongoUri = ''
+mongoose.connect(mongoUri, {
+    useNewUrlParser: true,
+    useCreateIndex: true
+});
+
+mongoose.connection.on('connected', () => {
+    console.log('Connected to mongo instance');;
+});
+mongoose.connection.on('error', (err) => {
+    console.error('Error', err);
+});
+
+app.get('/',(req, res) => {
+    res.send('Hi there!');
+});
+
+app.listen(3000, () => {
+    console.log('Listening on port 3000');
+});
